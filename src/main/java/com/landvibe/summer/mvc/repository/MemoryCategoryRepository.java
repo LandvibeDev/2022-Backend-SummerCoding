34537da67217f3fc0ff59999273442be7bdd79cc
jiwon1217 <koagjw@gmail.com>
2022-08-22 14:38:45 +0900

    변수명 변경

--- src/main/java/com/landvibe/summer/mvc/repository/MemoryCategoryRepository.java
+++ src/main/java/com/landvibe/summer/mvc/repository/MemoryCategoryRepository.java
@@ -7,30 +7,30 @@ import java.util.*;
 
 @Repository
 public class MemoryCategoryRepository implements CategoryRepository {
-    private static final Map<Long, Category> categories = new HashMap<>();
+    private static final Map<Long, Category> categoryDb = new HashMap<>();
     private static long sequence = 0L;
 
     @Override
     public void save(Category category) {
         category.setId(++sequence);
         category.setCount(0);
-        categories.put(category.getId(), category);
+        categoryDb.put(category.getId(), category);
     }
 
     @Override
     public Optional<Category> findById(Long id) {
-        return Optional.ofNullable(categories.get(id));
+        return Optional.ofNullable(categoryDb.get(id));
     }
 
     @Override
     public Optional<Category> findByName(String name) {
-        return categories.values().stream()
+        return categoryDb.values().stream()
                 .filter(category -> category.getName().equals(name))
                 .findAny();
     }
 
     @Override
     public List<Category> findAll() {
-        return new ArrayList<>(categories.values());
+        return new ArrayList<>(categoryDb.values());
     }
 }
--- src/main/java/com/landvibe/summer/mvc/repository/MemoryProductRepository.java
+++ src/main/java/com/landvibe/summer/mvc/repository/MemoryProductRepository.java
@@ -8,30 +8,30 @@ import java.util.*;
 
 @Repository
 public class MemoryProductRepository implements ProductRepository {
-    private static final Map<Long, Product> products = new HashMap<>();
+    private static final Map<Long, Product> productDb = new HashMap<>();
     private static long sequence = 0L;
 
     @Override
     public void save(Product product) {
         product.setId(++sequence);
         product.setCreatedAt(LocalDateTime.now());
-        products.put(product.getId(), product);
+        productDb.put(product.getId(), product);
     }
 
     @Override
     public Optional<Product> findById(Long id) {
-        return Optional.ofNullable(products.get(id));
+        return Optional.ofNullable(productDb.get(id));
     }
 
     @Override
     public Optional<Product> findByName(String name) {
-        return products.values().stream()
+        return productDb.values().stream()
                 .filter(product -> product.getName().equals(name))
                 .findAny();
     }
 
     @Override
     public List<Product> findAll() {
-        return new ArrayList<>(products.values());
+        return new ArrayList<>(productDb.values());
     }
 }
