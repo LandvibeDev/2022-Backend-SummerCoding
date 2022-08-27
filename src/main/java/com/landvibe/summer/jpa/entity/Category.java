9a60a6ff5a679f6915a39f59132b9ac551a9254a
jiwon1217 <koagjw@gmail.com>
2022-08-27 12:14:03 +0900

    Category, Product Entity

---
+++ src/main/java/com/landvibe/summer/jpa/entity/Category.java
@@ -0,0 +1,33 @@
+package com.landvibe.summer.jpa.entity;
+
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@DynamicInsert
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @ColumnDefault("0")
+    private Integer count;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+
+    public void addProduct(Product product) {
+        product.setCategory(this);
+        getProducts().add(product);
+    }
+}
---
+++ src/main/java/com/landvibe/summer/jpa/entity/Product.java
@@ -0,0 +1,30 @@
+package com.landvibe.summer.jpa.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(name = "category_id")
+    private Long categoryId;
+    @Column(name = "category_name")
+    private String categoryName;
+    private String description;
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "category")
+    private Category category;
+}
