485c90f132d168f8ba6186b278706d90b3317087
jiwon1217 <koagjw@gmail.com>
2022-08-29 00:15:34 +0900

    ExceptionHandler를 통한 공통 예외처리

---
+++ src/main/java/com/landvibe/summer/jpa/exception/ExceptionHandlers.java
@@ -0,0 +1,18 @@
+package com.landvibe.summer.jpa.exception;
+
+import com.landvibe.summer.jpa.dto.response.PostCommonRes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlers {
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<PostCommonRes> handler(Exception e) {
+        PostCommonRes postCommonRes = PostCommonRes.builder()
+                .code(-1)
+                .build();
+        e.printStackTrace();
+        return ResponseEntity.ok(postCommonRes);
+    }
+}
