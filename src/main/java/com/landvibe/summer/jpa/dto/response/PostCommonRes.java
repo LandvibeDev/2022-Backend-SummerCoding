d8afb40bc05c11d7a20156b85494c8e5aae2dce2
jiwon1217 <koagjw@gmail.com>
2022-08-28 22:40:58 +0900

    PostCommonRes 생성, 중복 되는 Response 삭제

--- src/main/java/com/landvibe/summer/jpa/dto/response/PostCommonRes.java
+++ src/main/java/com/landvibe/summer/jpa/dto/response/PostCommonRes.java
@@ -5,13 +5,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Map;
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class PostProductRes {
+public class PostCommonRes {
     private Integer code;
-    private Map<String, Long> result;
+    private Result result;
+
+    @Getter
+    @AllArgsConstructor
+    public static class Result {
+        private Long id;
+    }
 }
