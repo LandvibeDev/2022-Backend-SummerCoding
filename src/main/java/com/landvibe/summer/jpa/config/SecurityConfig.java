b6716462e0ebe661fd1c3d2f7711f942019d0a4b
jiwon1217 <koagjw@gmail.com>
2022-09-03 17:08:11 +0900

    SecurityConfig 설정

---
+++ src/main/java/com/landvibe/summer/jpa/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.landvibe.summer.jpa.config;
+
+import com.landvibe.summer.jpa.exception.JwtAccessDeniedHandler;
+import com.landvibe.summer.jpa.exception.JwtAuthenticationEntryPoint;
+import com.landvibe.summer.jpa.jwt.JwtSecurityConfig;
+import com.landvibe.summer.jpa.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final TokenProvider tokenProvider;
+    private final JwtAccessDeniedHandler accessDeniedHandler;
+    private final JwtAuthenticationEntryPoint authenticationEntryPoint;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .accessDeniedHandler(accessDeniedHandler)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/signup", "/api/login").permitAll()
+                .antMatchers("/api/category").hasAnyRole("ADMIN")
+                .antMatchers("/api/user/{name}").hasAnyRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
+
+        return httpSecurity.build();
+    }
+}
